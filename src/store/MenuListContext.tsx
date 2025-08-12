3e956a177f9a28494e377884df2a7e693b17231c
mooncw@hankookilbo.com <mooncw@hankookilbo.com>
2025-08-12 14:12:28 +0900

    fix: minor saved

--- src/store/MenuListContext.tsx
+++ src/store/MenuListContext.tsx
@@ -11,6 +11,7 @@ export const MenuListContext = createContext<IMenuListContextProps>({
       },
     ],
     selectedList: [],
+    answerList: [],
     // matchingTargets: [],
   },
   setData: () => {},
