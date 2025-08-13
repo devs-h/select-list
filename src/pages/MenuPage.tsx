2de8b31663898708e2676aa331927cf9b90dc07d
uiwwsw <uiwwsw@hankookilbo.com>
2025-08-13 17:46:59 +0900

    chore: 뒤로가기

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -3,20 +3,20 @@ import Confetti from "react-confetti";
 import { MenuListContext } from "../store/MenuListContext";
 import { MenuList } from "../components/MenuList";
 import { SelectMenuList } from "../components/SelectMenuList";
+import { navigate } from "../utils/navigate";
 export function MenuPage() {
   const [isShowSelectMenuList, setIsShowSelectMenuList] = useState(false);
   const { isAllStrike, removeStorage } = useContext(MenuListContext);
-
+  const handleClear = () => {
+    removeStorage();
+    navigate("/");
+  };
   return (
     <>
       {isAllStrike && <Confetti />}
       <div className="menu-list-container">
         <div className="menu-list-container-header">
-          <button
-            type="button"
-            className="button-reset"
-            onClick={() => removeStorage()}
-          >
+          <button type="button" className="button-reset" onClick={handleClear}>
             초기화
           </button>
           {/* 설정 버튼 - 전체 리스트 Show/Hide */}
